a9403af28d3f8b79ed06c84f878956e69f9e0ef9
90090 <ethanligi@gmail.com>
2026-06-03 15:02:25 -0400

    SEO plus backend

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   output: 'static',
+  site: 'https://bigdipperseymour.com',
 
   vite: {
     plugins: [tailwindcss()],
